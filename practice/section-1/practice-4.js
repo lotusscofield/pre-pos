4826d538e3f44f11d109d76e835f6dbc0d4c5e52
sissie <sissiescofield@outlook.com>
2015-07-21 13:58:50 +0800

    1

--- practice/section-1/practice-4.js
+++ practice/section-1/practice-4.js
@@ -1,12 +1,15 @@
 function collect_same_elements(collection_a, object_b) {
 
-  var collection_a1 = [];
- 	for (var i = 0; i < collection_a.length; i++) {
-    	collection_a1.push(collection_a[i].key);
+  var collection_c = [] ;
+
+  for (var i = 0; i < collection_a.length; i++) {
+    collection_c.push(collection_a[i].key);
 	}
 
-	return collection_a1.filter(function(item) {
-		 return(object_b.value.indexOf(item) !== -1);
-     });
-	}
+  return object_b.value.filter(function(item) {
+    return(collection_c.indexOf(item) !== -1) ;
+    }) ;
+  }
+
 }
+
